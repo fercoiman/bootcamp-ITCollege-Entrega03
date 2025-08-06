a364e8a8cf2e7c6adfc2043e3172cd200e567990
fercoiman <fcoiman@frba.utn.edu.ar>
2025-08-06 19:24:16 -0300

    agrego maquetado calculadora y funciones aritmeticas basicas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bootcamp-ITCollege-Entrega03
-bootcamp-ITCollege-Entrega02
+
+##Ejercicio 03 – JS
+
+Realizar un programa en Javascript, HTML5 y CSS3 que implemente unacalculadora con sus operaciones básicas: suma, resta, multiplicación y división.
+Crear la estructura HTML y los estilos necesarios para obtener el siguiente layout:
+
+El display de entrada de números y resultado no debe permitir más de 13caracteres ingresados o representados.La calculadora debe ser capaz de operar con números decimales.Al hacer click sobre el display se reiniciará el proceso de cálculo.
---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+var resultado = 0;
+
+function main() {
+    return;
+}
+
+function suma(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+function resta(arg1, arg2) {
+    return arg1 - arg2;
+}
+
+function multiplicacion(arg1, arg2) {
+    return arg1 * arg2;
+}
+
+function division(arg1, arg2) {
+    try {
+        if (arg2 == 0) {
+            throw "El divisor debe ser distinto de cero";
+        }
+        else {
+            var res = arg1 / arg2;
+        }
+    }
+    catch (err) {
+        alert("Error : " + err + "!!");
+        exit;
+    }
+    return res;
+}
+
+
+
+
+function calcular(operacion, operandoA, operandoB) {
+    return operacion(operandoA, operandoB);
+    }
+
+function mostrarResultado(){
+    var res = calcular
+}
+
+document.getElementById("display").innerHTML = <h2>toString.resultado</h2>;
+
+var res = calcular(suma, 3, 33);
+alert(res);
