a8410e63bcd325382dd493141223e2f7cd8fe31a
fercoiman <fcoiman@frba.utn.edu.ar>
2025-08-16 22:23:43 -0300

    agregao mas logica a la calcualdora falta resolver operacione encadenadas

--- js/main.js
+++ js/main.js
@@ -1,150 +1,43 @@
 
-var resultado = 0;
-var displayContent = '';
-
-// agregados ***//
-
 const display = document.querySelector(".display");
 const botones = document.querySelectorAll(".btn");
 
-botones.forEach(btn =>{
-    btn.addEventListener( 'click', buttonClick);});
+botones.forEach(btn =>{btn.addEventListener( 'click', buttonClick);});
+display.addEventListener("click",resetDisplay);
+let flag = true;
 
 function buttonClick (evt) {
     const ingreso = evt.target.textContent;
-    if(display.textContent === '0'){
+    
+   
+        
+    if(display.textContent === '0' || display.textContent === "Error!" || !flag ){
         display.textContent = ingreso;
+        flag = true;
     }
-    else{
-        display.textContent += ingreso;
+    else {
+        const maxChars = 13
+        display.textContent.length < maxChars ? 
+        display.textContent += ingreso : display.textContent.slice(0, -1);
+        flag = true;
     }
-}
 
-function limitarAnchoDisplay(num) {
-    var maxChars = 13;
-    const str = String(num);
-    return str.length <= maxChars ? str : str.slice(0, maxChars);
-}
-
-  //b.addEventListener( 'click', () => { console.log(b.textContent) });
-
-
-/*
-function main() {
-    return;
-}
-
-function suma(arg1, arg2) {
-    return arg1 + arg2;
-}
-
-function resta(arg1, arg2) {
-    return arg1 - arg2;
-}
-
-function multiplicacion(arg1, arg2) {
-    return arg1 * arg2;
-}
-
-function division(arg1, arg2) {
-    try {
-        if (arg2 == 0) {
-            throw "El divisor debe ser distinto de cero";
+    if( display.textContent.slice(-1) === "="){
+        try{
+            let resultado = eval(display.textContent.slice(0,-1));
+            let res = String(resultado);
+            display.textContent = res.slice(0,12);
+            flag = false;    
+            }
+        catch{
+             display.textContent = "Error!";
+            }
         }
-        else {
-            var res = arg1 / arg2;
-        }
-    }
-    catch (err) {
-        alert("Error : " + err + "!!");
-        exit;
-    }
-    return res;
-}
-
-
-function uno(){
-    displayContent += '1';
-    console.log(displayContent)
-    return displayContent;
-}
-
-function dos(){
-    displayContent += '2';
-    return displayContent;
-}
-
-function tres(){
-    displayContent += '3';
-    return displayContent;
-}
-
-function cuatro(){
-    displayContent += '4';
-    return displayContent;
-}
-
-function cinco(){
-    displayContent += '5';
-    return displayContent;
-}
-
-function seis(){
-    displayContent += '6';
-    return displayContent;
-}
-
-
-function ocho(){
-    displayContent += '8';
-    return displayContent;
-}
-
-function nueve(){
-    displayContent += '9';
-    return displayContent;
-}
-
-function cero(){
-    if(displayContent.length >= 1){
-        displayContent += '0';
-    }
-    return displayContent;
-}
-
-
-
-
-function calcular(operacion, operandoA, operandoB) {
-    return operacion(operandoA, operandoB);
+    return;  
     }
 
 
-
-function mostrarResultado(){
-    const displayResultado = document.getElementById('display');
-    var normalizado = limitarAnchoDisplay(displayContent);
-    displayResultado.innerText = `${normalizado}`;
+function resetDisplay(evt){
+    const rst = evt.target.textContent;
+    display.textContent = '0';
 }
-
-function limitarAnchoDisplay(num) {
-    var maxChars = 13;
-    const str = String(num);
-    return str.length <= maxChars ? str : str.slice(0, maxChars);
-}
-
-function limpiarDisplay(){
-    const disp = document.getElementById('display');
-    disp.addEventListener('dblclick', resetDisplay());
-    return
-}
-
-function resetDisplay(){
-    displayContent = '88'
-    console.log(displayContent)
-    return displayContent
-}
-
-mostrarResultado();*/
-//var res = calcular(suma, 3, 33);
-//alert(res);
