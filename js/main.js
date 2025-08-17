873a514c4d6ca5729a9743893ff96b82544b1390
fercoiman <fcoiman@frba.utn.edu.ar>
2025-08-16 22:37:17 -0300

    resuelvo error operaciones encadenadas. Version final

--- js/main.js
+++ js/main.js
@@ -4,30 +4,25 @@ const botones = document.querySelectorAll(".btn");
 
 botones.forEach(btn =>{btn.addEventListener( 'click', buttonClick);});
 display.addEventListener("click",resetDisplay);
-let flag = true;
 
 function buttonClick (evt) {
     const ingreso = evt.target.textContent;
     
-   
-        
-    if(display.textContent === '0' || display.textContent === "Error!" || !flag ){
+    if(display.textContent === '0' || display.textContent === "Error!" ){
         display.textContent = ingreso;
-        flag = true;
     }
+
     else {
         const maxChars = 13
         display.textContent.length < maxChars ? 
         display.textContent += ingreso : display.textContent.slice(0, -1);
-        flag = true;
-    }
+        }
 
     if( display.textContent.slice(-1) === "="){
         try{
             let resultado = eval(display.textContent.slice(0,-1));
             let res = String(resultado);
             display.textContent = res.slice(0,12);
-            flag = false;    
             }
         catch{
              display.textContent = "Error!";
